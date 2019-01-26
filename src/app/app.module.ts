ea8fc1b6d51710dca03a02b127cab7113119a4de
israel <israel.bejarano.a@gmail.com>
2019-01-26 14:41:21 +0100

    add drag basic functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ScrollingModule } from '@angular/cdk/scrolling'; // virtual scroll
+import { DragDropModule } from '@angular/cdk/drag-drop'; // basic drag
+
 
 
 import { AppComponent } from './app.component';
 import { VirtualComponent } from './components/virtual/virtual.component';
+import { DragComponent } from './components/drag/drag.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VirtualComponent
+    VirtualComponent,
+    DragComponent
   ],
   imports: [
     BrowserModule,
-    ScrollingModule
+    ScrollingModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
